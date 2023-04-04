fa1d991d8887539f726680eec05f554124bbbebe
Rhys Smoker <r.smoker@gmail.com>
2023-04-03 22:02:18 -0700

    updated about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -12,17 +12,20 @@ export default function AboutMe() {
 				id="rhys-portrait"
 			/>
 			<p align="left">
-				I am a life-long learner who loves attaining new skills and working on
-				teams. I am both hard-working and creative. My undergraduate degree from
-				Cal Poly Humboldt was Oceanographic Science with a Visual Arts minor. I
-				spent my 20's studying marine ecosystems and my 30's teaching math and
-				science in public and private schools. My travels took me from the
-				Channel Islands to the remote Pribilof Islands of the Bering Sea. I'm
-				now settled in Bellingham WA, where I live with my wife and young
-				children. I enjoy coding, spending time with my family, and building
-				things. I'm looking for a career where I can continually learn and
-				master new systems, and where my work-ethic and desire to help others is
-				valued.
+				As a lifelong learner, I am always eager to acquire new skills and work
+				collaboratively with others. I pride myself on being both hardworking
+				and creative. My academic background includes an undergraduate degree in
+				Oceanographic Science with a Visual Arts minor from Cal Poly Humboldt. I
+				spent my 20s studying marine ecosystems, and my 30s teaching math and
+				science in public and private schools. Throughout my career, I have had
+				the opportunity to travel extensively, from the Channel Islands to the
+				remote Pribilof Islands of the Bering Sea. Currently, I am settled in
+				Bellingham, WA, where I live with my wife and young children. When I'm
+				not spending quality time with my family, I enjoy coding and building
+				things. I am passionate about pursuing a career where I can continuously
+				learn and master new systems. I take pride in my strong work ethic and
+				desire to help others, and I am seeking a work environment where these
+				values are appreciated and valued.
 			</p>
 		</div>
 	);
