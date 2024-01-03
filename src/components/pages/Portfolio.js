f167d8afbaa84a00b4670cf12827da03a4292847
Rhys Smoker <r.smoker@gmail.com>
2024-01-03 11:04:45 -0800

    added a project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,7 +7,7 @@ const projects = [
 	{
 		Id: 1,
 		title: "REACT HOOKS Bananarama Grams",
-		subtitle: "A front-end crossword puzzle-like game.",
+		subtitle: "A front-end crossword puzzle game element simulation.",
 		description:
 			"A React app showcasing use of useState, useEffect, and useRef and custom CSS for visual effects. The game partially simulates a popular crossword-style tabletop game.",
 		repo: "https://github.com/MERN-ing-the-midnight-oil/tiles-game",
@@ -30,32 +30,6 @@ const projects = [
 		liveWeb: true,
 	},
 	{
-		Id: 3,
-		title: "Employee Tracker!",
-		subtitle: "A CLI tool for managing a company's employee database.",
-		description:
-			"Highlights the synergy of Node.js with Inquirer for a responsive command-line interface. Data management is efficiently handled using MySql.",
-		repo: "https://github.com/MERN-ing-the-midnight-oil/Employee-Tracker",
-		deployed_URL:
-			"https://drive.google.com/file/d/1iJv_3YIaGsj8JPUmZd-GV1ZTAgd9grn9/view",
-		image:
-			"https://cdn.midjourney.com/1a351108-b045-4a0f-9a79-369ef60de8b9/grid_0.png",
-		liveWeb: false,
-	},
-	{
-		Id: 4,
-		title: "What's in Your Cart?",
-		subtitle: "Back-end for an e-commerce site.",
-		description:
-			"The foundation of an e-commerce platform, emphasizing ORM techniques using an Express.js API, Sequelize, and MySQL.",
-		repo: "https://github.com/MERN-ing-the-midnight-oil/E-Commerce-Back-End",
-		deployed_URL:
-			"https://drive.google.com/file/d/1LqEl-L1l1fmLKjAZYBI0TBsrN4bHrP-Z/view",
-		image:
-			"https://cdn.midjourney.com/c218358e-91e1-40da-b2ab-577c3fc9705d/grid_0.png",
-		liveWeb: false,
-	},
-	{
 		Id: 5,
 		title: "Genre Genie",
 		subtitle: "A movie suggestion platform based on user-selected genres.",
@@ -67,42 +41,6 @@ const projects = [
 			"https://cdn.midjourney.com/a0dcda47-f18b-4887-9d50-578a4f6a1aed/grid_0.png",
 		liveWeb: true,
 	},
-	// {
-	// 	Id: 6,
-	// 	title: "Blogging with Handlebars",
-	// 	subtitle: "A Content Managment System built from scratch.",
-	// 	description:
-	// 		"This app demonstrates my use of the Model-View-Controller architechtural paradigm. It uses Handlebars.js as the templating language, Sequelize as the ORM(Object Relational Mapping), and the express-session npm package for authentification. ",
-	// 	repo: "https://github.com/MERN-ing-the-midnight-oil/MVC-Tech-Blog",
-	// 	deployed_URL: "https://dashboard.heroku.com/apps/stormy-gorge-58008",
-	// 	image:
-	// 		"https://cdn.midjourney.com/f7f033e3-764a-4428-8e47-c7887f972597/grid_0.png",
-	// 	liveWeb: false,
-	// },
-	{
-		Id: 7,
-		title: "Mongoose API",
-		subtitle: "API for social networking features.",
-		description:
-			"A back-end API for social networking features, built using Express.js, MongoDB, and Mongoose ODM.",
-		repo: "https://github.com/MERN-ing-the-midnight-oil/Social-Network-API",
-		deployed_URL: "https://watch.screencastify.com/v/pOD8r52BeDaPn99UDrVn", //todo do a new walkthrough
-		image:
-			"https://media.discordapp.net/attachments/1042540591650717738/1083449688356835388/Rhys_Smoker_a_mongoose_wearing_a_monocle_as_an_old-fashioned_bo_b19c0c11-8903-4eb9-8f17-ce7004c1fdd4.png?width=1051&height=1051", //todo take a screenshot
-		liveWeb: false,
-	},
-	{
-		Id: 7,
-		title: "Wall of Text",
-		subtitle: "An offline text editor with auto-save capabilities.",
-		description:
-			"Exemplifies the power of a Progressive Web App (PWA) using tools like webpack, workbox, babel, and the WebpackPwaManifest plug-in.",
-		repo: "https://github.com/MERN-ing-the-midnight-oil/PWA-Text-Editor ",
-		deployed_URL: "https://damp-coast-38671.herokuapp.com/",
-		image:
-			"https://cdn.midjourney.com/c07d972b-8931-4161-9b7b-e3618d096e84/grid_0.png",
-		liveWeb: false,
-	},
 	{
 		Id: 8,
 		title: "Mail-Me-Seeds",
@@ -113,7 +51,7 @@ const projects = [
 		deployed_URL: "https://mail-me-seeds.herokuapp.com/",
 		image:
 			"https://cdn.midjourney.com/dfc5bb94-1992-4f82-9c0e-ee38621a0bef/grid_0.png",
-		fullStack: false,
+		liveWeb: true,
 	},
 	{
 		Id: 9,
@@ -153,6 +91,70 @@ const projects = [
 		image:
 			"https://cdn.midjourney.com/7e5e1040-e3a8-4bf1-8f2d-227d2751aeef/0_0.webp",
 		liveWeb: true,
+	},
+	{
+		Id: 11,
+		title: "NOAA Fisheries BSAI/GOA Bycatch Rates Widget",
+		subtitle:
+			"Full Stack MERN Web Application with RESTful and GraphQL Data Handling",
+		description:
+			"A comprehensive full stack MERN application demonstrating dynamic data fetching and visualization for NOAA fisheries bycatch rates. Features include viewing catch records by year or date range, filtering by vessel and season, and charting catch records using Material-UI and Recharts for a responsive UI.",
+		repo: "https://github.com/MERN-ing-the-midnight-oil/bycatch-rates-by-week-and-vessel",
+		deployed_URL:
+			"https://github.com/MERN-ing-the-midnight-oil/bycatch-rates-by-week-and-vessel#readme",
+		image:
+			"https://cdn.midjourney.com/ec411195-821e-4f8d-8e9e-0ff324d3b9fb/0_0.webp",
+		liveWeb: true,
+	},
+	{
+		Id: 3,
+		title: "Employee Tracker!",
+		subtitle: "A CLI tool for managing a company's employee database.",
+		description:
+			"Highlights the synergy of Node.js with Inquirer for a responsive command-line interface. Data management is efficiently handled using MySql.",
+		repo: "https://github.com/MERN-ing-the-midnight-oil/Employee-Tracker",
+		deployed_URL:
+			"https://drive.google.com/file/d/1iJv_3YIaGsj8JPUmZd-GV1ZTAgd9grn9/view",
+		image:
+			"https://cdn.midjourney.com/1a351108-b045-4a0f-9a79-369ef60de8b9/grid_0.png",
+		liveWeb: false,
+	},
+	{
+		Id: 4,
+		title: "What's in Your Cart?",
+		subtitle: "Back-end for an e-commerce site.",
+		description:
+			"The foundation of an e-commerce platform, emphasizing ORM techniques using an Express.js API, Sequelize, and MySQL.",
+		repo: "https://github.com/MERN-ing-the-midnight-oil/E-Commerce-Back-End",
+		deployed_URL:
+			"https://drive.google.com/file/d/1LqEl-L1l1fmLKjAZYBI0TBsrN4bHrP-Z/view",
+		image:
+			"https://cdn.midjourney.com/c218358e-91e1-40da-b2ab-577c3fc9705d/grid_0.png",
+		liveWeb: false,
+	},
+	{
+		Id: 7,
+		title: "Mongoose API",
+		subtitle: "API for social networking features.",
+		description:
+			"A back-end API for social networking features, built using Express.js, MongoDB, and Mongoose ODM.",
+		repo: "https://github.com/MERN-ing-the-midnight-oil/Social-Network-API",
+		deployed_URL: "https://watch.screencastify.com/v/pOD8r52BeDaPn99UDrVn", //todo do a new walkthrough
+		image:
+			"https://media.discordapp.net/attachments/1042540591650717738/1083449688356835388/Rhys_Smoker_a_mongoose_wearing_a_monocle_as_an_old-fashioned_bo_b19c0c11-8903-4eb9-8f17-ce7004c1fdd4.png?width=1051&height=1051", //todo take a screenshot
+		liveWeb: false,
+	},
+	{
+		Id: 7,
+		title: "Wall of Text",
+		subtitle: "An offline text editor with auto-save capabilities.",
+		description:
+			"Exemplifies the power of a Progressive Web App (PWA) using tools like webpack, workbox, babel, and the WebpackPwaManifest plug-in.",
+		repo: "https://github.com/MERN-ing-the-midnight-oil/PWA-Text-Editor ",
+		deployed_URL: "https://damp-coast-38671.herokuapp.com/",
+		image:
+			"https://cdn.midjourney.com/c07d972b-8931-4161-9b7b-e3618d096e84/grid_0.png",
+		liveWeb: false,
 	},
 ];
 const liveProjects = projects.filter((item) => item.liveWeb);
